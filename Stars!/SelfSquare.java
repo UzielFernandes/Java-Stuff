1e6ccbbc56549e0b775500d1b01e3cc8d22d9ab0
Uziel Fernandes <uzielfern07@gmail.com>
2026-08-13 18:44:34 +0530

    added Description

--- Stars!/SelfSquare.java
+++ Stars!/SelfSquare.java
@@ -1,3 +1,7 @@
+//Square of Stars: Self Made
+
+//Enter a number and that many rows and column of stars generate, essentially forming a square
+
 import java.util.Scanner;
 
 public class SelfSquare {
@@ -9,16 +13,16 @@ public class SelfSquare {
         
         System.out.println(noOfStars);
 
-        sub(noOfStars);
+        sub(noOfStars); //to call the method
     }
 
     public static void sub(int noOfStars) {
 
-        int rows = noOfStars;
-        int starIteration = noOfStars;
+        int rows = noOfStars; //no of rows = no input by user
+        int starIteration = noOfStars; //a replenishing variable which = to no input by user
 
-        while (rows != 0) {
-            while(starIteration != 0) {
+        while (rows != 0) { //while rows is not 0 (each iteration rows decreases by 1!)
+            while(starIteration != 0) { //to print stars
                 System.out.print("*");
                 starIteration--;
             }
