c562f29ccb88216b0d86569fe303ab14e7f84edf
Uziel Fernandes <uzielfern07@gmail.com>
2026-08-13 18:49:31 +0530

    Added Description

--- Stars!/Square.java
+++ Stars!/Square.java
@@ -1,29 +1,33 @@
+//Square of Stars: Almost Self Made
+
+//Enter a number and that many rows and column of stars generate, essentially forming a square
+
 import java.util.Scanner;
 
 public class Square{
-    public static void main(String[] args) {
+    public static void main(String[] args) { //whole point of main method is to get input
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("how many star ka square?: ");
         int inputGiven = Integer.valueOf(scanner.nextLine());
 
-        printSquare(inputGiven);        
+        printSquare(inputGiven); //send input number to printsquare method
     }
 
-    public static void printStars(int state) {
+    public static void printStars(int state) { //to print stars given by the skeleton of printsquare
         while (state!=0) {
             System.out.print("*");
             state--;
         }
     }
 
-    public static void printSquare(int inputGiven) {
-        int rows = inputGiven;
+    public static void printSquare(int inputGiven) { //method to, well, print the square (the structure)
+        int rows = inputGiven; //no of rows = input given
 
         while (rows != 0) {
-            int state = inputGiven;
-            printStars(state);
-            System.out.println();
+            int state = inputGiven; //as rows decreases by one each iteration, have state variable which holds input no permanently
+            printStars(state); //send to print stars method
+            System.out.println(); //to leave a line after each row
 
             rows--;
         }
